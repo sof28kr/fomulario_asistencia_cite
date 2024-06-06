3658b1e954a2054757e0913aeb8b4b9b6a39b7b3
sof28kr <sofia28kr@gmail.com>
2024-06-05 19:13:51 -0500

    vamos avanzando muy bien

--- lib/Views/Participantes/EditarParticipantes.dart
+++ lib/Views/Participantes/EditarParticipantes.dart
@@ -205,7 +205,8 @@ class _EditarParticipantesState extends State<EditarParticipantes> {
                         child: isBase64String(context
                                 .watch<providerParticipanteId>()
                                 .provfirmaid)
-                            ? Expanded(
+                            ? SizedBox(
+                                height: 200,
                                 child: AspectRatio(
                                   aspectRatio: 2,
                                   child: Image.memory(
